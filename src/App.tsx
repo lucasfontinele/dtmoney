91792ed00d72e4f4db327154f927c0a66fe153a8
Lucas Fontinele <lfontinele07@gmail.com>
2021-05-12 09:00:46 -0300

    Implements Transactions Table & Miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,26 @@ import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 
 import { GlobalStyle } from './styles/Global';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    });
+  }
+});
 
 function App() {
   return (
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,46 +1,59 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
+type Transaction = {
+  id: number;
+  title: string;
+  amount: number;
+  type: 'deposit' | 'outcome',
+  category: string;
+  createdAt: Date;
+};
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api.get('/transactions')
+      .then(({ data }) => setTransactions(data));
+  }, []);
+
   return (
     <Container>
-      <table>
-        <thead>
-          <tr>
-            <th>
-              Título
-            </th>
-            <th>
-              Valor
-            </th>
-            <th>
-              Categoria
-            </th>
-            <th>
-              Data
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000,00</td>
-            <td>Desenvolvimento</td>
-            <td>10/05/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$ 1.100,00</td>
-            <td>Casa</td>
-            <td>10/05/2021</td>
-          </tr>
-          <tr>
-            <td>Parcela Carro</td>
-            <td className="withdraw">- R$ 400,00</td>
-            <td>Carro</td>
-            <td>12/05/2021</td>
-          </tr>
-        </tbody>
-      </table>
+      {transactions.length === 0 && (
+        <p>Nenhuma transação foi registrada.</p>
+      )}
+      {transactions.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Título
+              </th>
+              <th>
+                Valor
+              </th>
+              <th>
+                Categoria
+              </th>
+              <th>
+                Data
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>Desenvolvimento de website</td>
+                <td className="deposit">R$ 12.000,00</td>
+                <td>Desenvolvimento</td>
+                <td>10/05/2021</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </Container>
   );
 }
